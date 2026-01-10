65074892569a76ac2b112e1b15e4a60de7fcecba
Ilona Bondarovich <ilona.bondarovich@gmail.com>
2026-01-11 02:17:11 +0300

    Add UserDefaults

--- TodoApp_uikit/Controllers/SettingsViewController.swift
+++ TodoApp_uikit/Controllers/SettingsViewController.swift
@@ -12,9 +12,11 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var appThemeLabel: UILabel!
     @IBOutlet weak var settingsTitleLabel: UILabel!
     @IBOutlet weak var modalView: UIView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpView()
     }
     
     override func viewDidLayoutSubviews() {
@@ -34,10 +36,36 @@ class SettingsViewController: UIViewController {
         
         if sender.selectedSegmentIndex == 0 {
             window?.overrideUserInterfaceStyle = .light
+            saveThemeMode(themeIndex: 0)
         } else if sender.selectedSegmentIndex == 1 {
             window?.overrideUserInterfaceStyle = .dark
-        } else if sender.selectedSegmentIndex == 2 {
+            saveThemeMode(themeIndex: 1)
+        } else {
             window?.overrideUserInterfaceStyle = .unspecified
+            saveThemeMode(themeIndex: 2)
         }
+    }
+    
+    func setUpView() {
+        let window = UIApplication.shared.connectedScenes.flatMap {
+            ($0 as? UIWindowScene)?.windows ?? []
+        }.first { $0.isKeyWindow }
+        
+        if let window = window {
+            switch window.overrideUserInterfaceStyle {
+            case .light:
+                segmentedControl.selectedSegmentIndex = 0
+            case .dark:
+                segmentedControl.selectedSegmentIndex = 1
+            case .unspecified:
+                segmentedControl.selectedSegmentIndex = 2
+            default:
+                segmentedControl.selectedSegmentIndex = 2
+            }
+        }
+    }
+    
+    func saveThemeMode(themeIndex index: Int) {
+        UserDefaults.standard.set(index, forKey: "interfacePreference")
     }
 }
